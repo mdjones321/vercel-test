36c41c2ec7c5505949673dfd13f7d32eb933083b
Corinne Jones <jonescorinne6@gmail.com>
2024-09-03 11:09:25 -0600

    feat: initial web app creation

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Sidebar from './SideBar';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  //  <h1 className="text-4xl text-blue-500"> Vite Project</h1>
+   <Sidebar></Sidebar>
+  );
+}
+
+export default App
